06cd93d2563475a72f6691a9520b9065e4da05f3
rizwanp123 <md.rizwan960@gmail.com>
2021-10-02 14:17:38 +0530

    Add groups, parameters, listeners

--- src/test/java/driver/BaseClass.java
+++ src/test/java/driver/BaseClass.java
@@ -1,10 +1,9 @@
 package driver;
-import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
-	
 	//This is a base class
 	
 	public static WebDriver driver;
